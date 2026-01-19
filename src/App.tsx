c2a768fdfc4583eae745402b62d1df5bdeb2d93c
Shreyash Chougule <shreyash.chougule@thinkitive.com>
2026-01-19 15:30:26 +0530

    Dashborad done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { LoginPage } from "./Auth/LoginPage"
+import ProtectedRoute from "./Routes/ProtectedRoute"
+import DashBoard from "./components/DashBoard"
 
 function App() {
 
   return (
     <>
-      <LoginPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <DashBoard />
+              </ProtectedRoute>
+            } />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
